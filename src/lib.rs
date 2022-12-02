c918806df6a24103429e0e6332d83fb5dc80f970
Ulf Lilleengen <lulf@redhat.com>
2022-12-02 18:05:15 +0100

    feat: afit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![feature(type_alias_impl_trait)]
+#![feature(async_fn_in_trait)]
+#![allow(incomplete_features)]
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 
